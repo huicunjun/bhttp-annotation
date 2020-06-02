440555bc7b2aaa2664fc9761ab3ad919720a9010
威威君” <1214585092@qq.com>
2020-06-02 10:33:32 +0800

    2

---
+++ bhttp-annotation/src/main/java/com/ldw/bhttp_annotation/Request.java
@@ -0,0 +1,15 @@
+package com.ldw.bhttp_annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 自定义返回Response
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.CLASS)
+public @interface Request {
+    String name() default "";
+}
